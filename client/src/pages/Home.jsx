a9afa4f9abefe4ff494cd50e54c5e11c043385da
HARI VISHVANATH S <harivishvanath7@gmail.com>
2026-04-25 10:37:47 +0530

    feat(pages): Initialised Pages for Home, Menu, Login.

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+
+const Home = () => {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-background">
+      <h1 className="text-4xl font-bold text-accent">
+        Restaurant UI Ready 🔥
+      </h1>
+    </div>
+  )
+}
+
+export default Home
