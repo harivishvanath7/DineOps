5df442c92270e912d7dd8486cd69441e33ad275f
HARI VISHVANATH S <harivishvanath7@gmail.com>
2026-04-25 12:36:51 +0530

    feat(model): Added User model.

---
+++ server/models/User.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      enum: ["admin", "staff"],
+      default: "staff",
+    },
+  },
+  { timestamps: true },
+);
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
